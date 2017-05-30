9284abb23d5b4cb1dbad18a0a2efa094e2aadea4
uzair <uzair@techappssolutions.com>
2017-05-30 20:03:59 +0500

    change for heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCNC7rQDo3xqBypQoGQcHagm6m6U9jzvwo",
+    authDomain: "businesscontacts-eb3dc.firebaseapp.com",
+    databaseURL: "https://businesscontacts-eb3dc.firebaseio.com",
+    projectId: "businesscontacts-eb3dc",
+    storageBucket: "businesscontacts-eb3dc.appspot.com",
+    // messagingSenderId: "623001757792"
+  }
 };
